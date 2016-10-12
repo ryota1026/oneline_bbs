3100e0ac109fbb19e485980cb79d370b2a0a42cf
RYOTA TAJIMA <tajima1026@gmail.com>
2016-10-12 11:00:21 +0800

    一覧表示の実装

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -12,10 +12,20 @@
   $dbh->query('SET NAMES utf8'); 
   
   // SQl文の実行
-  $sql = 'INSERT INTO `posts`(`nickname`, `comment`) VALUES ("'.$nickname.'", "'.$comment.'")';
+  $sql = 'INSERT INTO `posts`(`id`,`nickname`, `comment`,`created`) VALUES (null,"'.$nickname.'", "'.$comment.'",now())';
   $stmt = $dbh->prepare($sql);
   $stmt->execute();
   
+  while (1) {
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($rec == false) {
+      break;
+  }
+  echo $rec['id'] . '<br>'; 
+  echo $rec['nickname'] . '<br>';
+  echo $rec['comment'] . '<br>';
+  echo '<hr>';
+ }
   // データベースを切断する
   $dbh = null;
 ?>
@@ -33,7 +43,40 @@
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
-    
 
 </body>
 </html>
+
+<?php
+  // ここにDBに登録する処理を記述する
+  $nickname = htmlspecialchars($_POST['nickname']);
+  $comment = htmlspecialchars($_POST['comment']);
+
+  // データベースへの接続
+  $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+  $user = 'root';
+  $password='';
+
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8'); 
+  
+  // SQl文の実行
+  $sql = 'SELECT * FROM `posts`';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+  
+  while (1) {
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($rec == false) {
+      break;
+  }
+  echo $rec['id'] . '<br>'; 
+  echo $rec['nickname'] . '<br>';
+  echo $rec['comment'] . '<br>';
+  echo $rec['created'] . '<br>';
+  echo '<hr>';
+ }
+  // データベースを切断する
+  $dbh = null;
+?>
+
